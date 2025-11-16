0033a41b9a5fece8dad4460f51f3e9c523d1fb3c
Manas Singh <singhmanas2918@gmail.com>
2025-11-17 01:00:40 +0530

    new tasks

---
+++ JavaScript/Practice Questions/script.js
@@ -0,0 +1,142 @@
+//  LEVEL 1 : Beginner Practice
+
+// Question 1 - print numbers from 1 to 10 
+// for(i = 1; i <= 10; i++ ){
+//     console.log(i)
+// }
+
+
+// Question 2 - print only even numbers from 1 to 20
+// for(i =1; i <= 20; i++){
+//     if(i % 2 === 0) {
+//         console.log(i)
+//     }
+// }
+
+
+// Questions 3 - Print number from 10 to 1
+// for(i = 10; i>=1 ; i--){
+//     console.log(i)
+// }
+
+
+// Questions 4 - Print the word yes 5 times
+// for(i= 1; i<= 5; i++){
+//     console.log("yes")
+// }
+
+
+// Question 5 -print whether numbr from 1 to 10 are even or odd
+// for(i = 1; i <= 10; i++){
+//     if(i % 2 === 0){
+//         console.log(`${i} is Even`)
+//     } else {
+//         console.log(`${i} is Odd`)
+//     }
+// }
+
+
+// Questions 6 - ask user for a number and say if it's positive or negstive
+// let num = prompt("Enter any Integer : ")
+// num = parseInt(num)
+// if(num < 0){
+//     console.log("Negative Number")
+// } else{
+//     console.log("Positive Number")
+// }
+
+
+// Question 7 - Ask user's age and check if eligible to vote
+// let age = prompt("Enter your age : ")
+// age = parseInt(age)
+// if (age >= 18) {
+//     console.log("Eligible for vote")
+// } else {
+//     console.log("Not eligible")
+// }
+
+
+// Question 8 - Print multilpication table of 5 
+// for(i = 1; i <= 10; i++ ) {
+//     console.log( `5 * ${i} = ${ 5 * i}`)
+// }
+
+// let num = prompt("Enter number: ")
+// console.log(`Table of ${num} : \n `)
+// for(i = 1; i <= 10; i++) {
+//     console.log(num * i)
+// }
+
+
+// Question 9 - Count how many numbers between 1 and 15 are greater than 8 
+// let count = 0
+// for(i= 1; i< 16; i++){
+//     if(i > 8){
+//         count++
+//     }
+// }
+// console.log(count)
+
+
+// Question 10 - Ask user for password and print access status
+// Hardcoded correct password. Compare with user input.
+// var pass = prompt("Enter Password : ")
+// if(pass === "manas"){
+//     console.log("Password is Correct.")
+// } else {
+//     alert("Enter correct password")
+//     console.error("Enter correct password.")
+// }
+
+// ---------------------------------------------------------------------------
+
+// Level 2 -slightly tougher but logical
+
+// Question 11 - Allow only 3 attempts to enter correct password
+// If user gets it right early, stop. If not → “Account locked”
+// let attempts = 0
+// let pass = "manas"
+// let enter = prompt("Enter Passwod")
+// attempts++
+// while(pass !== enter){
+//     if(attempts === 3) {
+//         console.error("account blocked")
+//         break
+//     } 
+//     enter = prompt("Enter Passwod")
+//     attempts++
+// }
+// if(pass === enter){
+//     console.log("password matched")
+// }
+
+
+
+// Question 12 - Ask user for words until they type “stop”. Count how many times they typed “yes”
+// Loop until "stop" is typed. Count "yes".
+
+
+
+// Question 13 - 
+
+
+// Question 14 - 
+
+
+// Question 15 - 
+
+
+// Question 16 - 
+
+
+// Question 17 - 
+
+
+// Question 18 - 
+
+
+// Question 19 - 
+
+
+// Question 20 - 
+
