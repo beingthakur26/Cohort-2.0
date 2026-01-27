3ab0f0dc191ca42a87ee1aad4ca4b1fefea9e149
Manas Singh <singhmanas2918@gmail.com>
2026-01-28 00:30:23 +0530

    new one

---
+++ Backend/lec-1/script.js
@@ -0,0 +1,11 @@
+let num = 25;
+
+const isEven = (number) => {
+    if (number % 2 === 0) {
+        console.log("The number is even.");
+    } else {
+        console.log("The number is odd.");
+    }
+}
+
+isEven(num);
