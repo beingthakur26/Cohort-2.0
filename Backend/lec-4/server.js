64a545d5c92c0ea55ebaafaa2d298334cc1c39b4
Manas Singh <singhmanas2918@gmail.com>
2026-02-09 20:53:31 +0530

    backend4

---
+++ Backend/lec-4/server.js
@@ -0,0 +1,9 @@
+/*
+    - server ko start krna
+*/
+
+const app = require("./src/app");
+
+app.listen(4000, () => {
+    console.log("Server started on port 4000");
+})
---
+++ Backend/lec-4/src/app.js
@@ -0,0 +1,25 @@
+/**
+    - server create krna 
+    - server ko config krna
+ */
+
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const pages = [];
+
+app.post("/pages", (req, res) => {
+    const page = req.body;
+    pages.push(page);
+    console.log(page);
+
+    res.json({ message: "Page added successfully" });
+});
+
+app.get("/pages", (req, res) => {
+    res.json(pages);
+});
+    
+module.exports = app;
