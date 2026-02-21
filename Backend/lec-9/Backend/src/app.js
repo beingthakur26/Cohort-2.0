79beb5e39a0b56abfc72c8b4a4cb2200aeec6007
Manas Singh <singhmanas2918@gmail.com>
2026-02-21 12:26:38 +0530

    backend 9

---
+++ Backend/lec-9/Backend/src/app.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const noteModel = require("./models/notes.model");
+const cors = require("cors");
+const path = require("path");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("./public"));
+
+/*
+    POST /notes
+    req.body => {title, content}
+*/
+app.post("/notes", async (req, res) => {
+    const { title, content } = req.body;
+    const note = await noteModel.create({
+        title,
+        content,
+    });
+    res.status(201).json({
+        message: "Note created successfully",
+        note,
+    });
+});
+
+/*
+    GET /notes
+*/
+app.get("/notes", async (req, res) => {
+    const notes = await noteModel.find();
+    res.status(200).json({
+        message: "Notes fetched successfully",
+        notes,
+    });
+});
+
+/*
+    DELETE /notes/:id
+    delete note with the from req.params.id
+*/
+app.delete("/notes/:id", async (req, res) => {
+    const { id } = req.params;
+    const note = await noteModel.findByIdAndDelete(id);
+    res.status(200).json({
+        message: "Note deleted successfully",
+        note,
+    });
+});
+
+/*
+    PATCH /notes/:id
+    update content of the note
+    update note with the from req.params.id
+*/
+app.patch("/notes/:id", async (req, res) => {
+    const { id } = req.params;
+    const { content } = req.body;
+    const note = await noteModel.findByIdAndUpdate(id, {
+        content,
+    });
+    res.status(200).json({
+        message: "Note updated successfully",
+        note,
+    });
+});
+
+app.use('*name', (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+});
+
+module.exports = app;
---
+++ Backend/lec-9/Frontend/src/App.jsx
@@ -0,0 +1,97 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const App = () => {
+
+  const [notes, setNotes] = useState([]);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const fetchNotes = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/notes");
+      setNotes(response.data.notes);
+    } catch (error) {
+      console.log("Error fetching notes:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchNotes()
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!title || !content) {
+      console.log("Both fields required");
+      return;
+    }
+    try {
+      await axios.post("http://localhost:3000/notes", {
+        title,
+        content
+      });
+      fetchNotes();
+      setTitle("");
+      setContent("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`http://localhost:3000/notes/${id}`);
+      fetchNotes();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleUpdate = async (id) => {
+    try {
+      await axios.patch(`http://localhost:3000/notes/${id}`, {
+        title,
+        content
+      });
+      fetchNotes();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <form className="create-note" onSubmit={handleSubmit}>
+        <input 
+          type="text" 
+          placeholder="Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input 
+          type="text" 
+          placeholder="Content"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        />
+        <button>Create Note</button>
+      </form>
+
+      <div className="notes">
+        {notes.map((note) => (
+          <div className="note" key={note._id}>
+            <h1>{note.title}</h1>
+            <p>{note.content}</p>
+            <div className="buttons">
+              <button onClick={() => handleDelete(note._id)}>Delete</button>
+              <button onClick={() => handleUpdate(note._id)}>Update</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default App
